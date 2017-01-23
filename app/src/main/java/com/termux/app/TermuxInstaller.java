0eb2318d53d060cd9aac79ba205867ab0223ec0a
Karl Semich <0xloem@gmail.com>
2017-01-23 20:25:38 +0000

    Super-quick hack for offline installation

--- app/src/main/java/com/termux/app/TermuxInstaller.java
+++ app/src/main/java/com/termux/app/TermuxInstaller.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.UserManager;
@@ -20,6 +21,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -82,8 +84,7 @@ final class TermuxInstaller {
                     final byte[] buffer = new byte[8096];
                     final List<Pair<String, String>> symlinks = new ArrayList<>(50);
 
-                    final URL zipUrl = determineZipUrl();
-                    try (ZipInputStream zipInput = new ZipInputStream(zipUrl.openStream())) {
+                    try (ZipInputStream zipInput = new ZipInputStream(openZipStream(activity))) {
                         ZipEntry zipEntry;
                         while ((zipEntry = zipInput.getNextEntry()) != null) {
                             if (zipEntry.getName().equals("SYMLINKS.txt")) {
@@ -168,12 +169,12 @@ final class TermuxInstaller {
     }
 
     /** Get bootstrap zip url for this systems cpu architecture. */
-    private static URL determineZipUrl() throws MalformedURLException {
+    private static InputStream openZipStream(Context context) throws IOException {
         String archName = determineTermuxArchName();
-        String url = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
-            ? "https://termux.org/bootstrap-" + archName + ".zip"
-            : "https://termux.net/bootstrap/bootstrap-" + archName + ".zip";
-        return new URL(url);
+        String zipUrl = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+            ? "N/bootstrap-" + archName + ".zip"
+            : "bootstrap-" + archName + ".zip";
+        return context.getAssets().open(zipUrl);
     }
 
     private static String determineTermuxArchName() {
